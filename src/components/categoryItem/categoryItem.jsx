efe0568d8d04eb947d89f5600174c6a431613606
Torbi Ronald Reagan <reagantamakloe@gmail.com>
2025-07-16 09:54:43 +0000

    Added CategoryItem Component

---
+++ src/components/categoryItem/categoryItem.jsx
@@ -0,0 +1,17 @@
+
+const CategoryItem = ({category}) => {
+    const {title, imageUrl} = category
+    return(
+        <div className="category-container">
+            <div style={{
+                backgroundImage: `url(${imageUrl})`
+              }} />
+            <div className="category-body-container"> 
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
