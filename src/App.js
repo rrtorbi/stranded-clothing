df74fb61a87249770f46ba69753e454d2c933329
Torbi Ronald Reagan <reagantamakloe@gmail.com>
2025-07-17 10:20:03 +0000

    routing with react router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-
-import Directory from './components/directory/directory-component';
+import { Routes,Route } from "react-router-dom"
+import Home from "./components/routes/home/home.component"
+import NavigationBar from "./components/routes/navigation/navigation.component"
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-
+  const Shop = () => {
+    return(
+      <h1>This is my fucken Shop</h1>
+    )
+  }
   return(
-    <div className="categories-container">
-      {
-        <Directory categories={categories} />
-      }
-    </div>
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
   )
 }
 
